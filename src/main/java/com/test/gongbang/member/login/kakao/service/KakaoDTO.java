b25f0c7e439a453fc1967a1ce2dd60adb523fc5f
Hailey Lim <lsuj104@naver.com>
2023-01-25 03:43:04 +0900

    카카오 로그인 recommit
    Signed-off-by: Hailey Lim <lsuj104@naver.com>

--- src/main/java/com/test/gongbang/member/login/kakao/service/KakaoDTO.java
+++ src/main/java/com/test/gongbang/member/login/kakao/service/KakaoDTO.java
@@ -1,2 +1,15 @@
-package com.test.gongbang.member.login.kakao.service;public class KakaoDTO {
+package com.test.gongbang.member.login.kakao.service;
+
+import lombok.Data;
+
+@Data
+public class KakaoDTO {
+    private long seq;
+    private String id; // kakao - email
+    private String nickname;
+    private String image;
+    private String lv;
+    private String tel;
+    private String name;
+    private String active;
 }
