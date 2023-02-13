8aef88a394fa0dd8a713983ac4ee1f1a065694be
tiger0827 <tiger980827@gmail.com>
2023-02-13 21:51:19 +0900

    feedview1
    Signed-off-by: tiger0827 <tiger980827@gmail.com>

--- src/main/java/com/test/gongbang/feed/controller/FeedController.java
+++ src/main/java/com/test/gongbang/feed/controller/FeedController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -23,7 +24,12 @@ public class FeedController {
     }
 
     @GetMapping("feed/feedview")
-    public String feedview(){
+    public String feedview(@RequestParam String seq , Model model){
+
+        FeedDTO dto = service.feedview(seq);
+
+        model.addAttribute("feedview", dto);
+
         return "feed/feedview";
 
     }
